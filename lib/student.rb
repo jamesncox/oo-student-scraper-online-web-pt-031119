7c0666a183c90e0a21f673f3152220b54632fd87
James Cox <jamesncox@yahoo.com>
2019-04-20 16:46:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,8 @@ class Student
   def initialize(student_hash)
     student_hash.each do |a, v|
       self.send("#{a}", v)
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
